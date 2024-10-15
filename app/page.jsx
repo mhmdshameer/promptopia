601560decebd19f9bed5f07f188ee9b87909cbd8
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-15 20:26:14 +0530

    feat(navbar): create necessary components and partially complete the navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import Feed from "@components/Feed";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      Home      
-    </div>
-  )
-}
+    <section className="w-full text-center">
+      <h1 className=" head_text text-center">
+        discover & share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered prompts</span>
+      </h1>
+      <p className="desc text-center">
+        promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+      </p>
+      <Feed/>
+    </section>
+  );
+};
 
-export default Home
-
+export default Home;
