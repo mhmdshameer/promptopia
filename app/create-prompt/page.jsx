5103cd096e4318357db11dcd8d835ed67fc17694
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-19 06:41:20 +0530

    feat(ui): add form for creating a new prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+'use client'; 
+import Form from 'components/Form'
+import React, { useState } from 'react'
 
 const CreatePrompt = () => {
+  const [submitting, useSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  })
+
+  const createPrompt = async (e) => {
+    
+  }
   return (
     <div>
-      CreatePrompt
+      <Form
+        type="Create"
+        post= {post}
+        setPost= {setPost}
+        submitting= {submitting}
+        handleSubmit= {createPrompt}
+      />
     </div>
   )
 }
