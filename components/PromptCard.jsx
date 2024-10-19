286b38b5dde12ead53ebc944d3e7e2127043081a
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-19 13:01:36 +0530

    feat(ui): start building PromptCard component with user info display

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
 
-const PromptCard = () => {
+import Image from "next/image";
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
-    <div>
-      PromptCard
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="user image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
+            <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PromptCard
+export default PromptCard;
