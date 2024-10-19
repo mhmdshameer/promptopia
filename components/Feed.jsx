c91f68d1e100812a516dec8879076fa222a805c0
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-19 10:37:07 +0530

    feat(api): add GET endpoint to fetch all prompts with creator details

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { useEffect, useState } from "react"
+import PromptCard from "./PromptCard"
 
 const PromptCardList = ({data, handleTagClick}) => {
   return (
     <div className="mt-16 prompt_layout">
-       {data.map((post)=>(
-        <PromptCardList 
+       {data?.map((post)=>(
+        <PromptCard 
           key={post.id}
           post={post}
           handleTagClick={handleTagClick}
