1c5ea088584d5f946e2198a95ed00ffebd09e279
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-19 10:26:34 +0530

    feat(feed): implement fetchPosts and display prompts with search functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,13 +1,37 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+       {data.map((post)=>(
+        <PromptCardList 
+          key={post.id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+       ))}
+    </div>
+  )
+}
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('')
-
+  const [posts, setPosts] = useState([])
   const handleSearchChange = (e) =>{
-    
+
   }
+
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      const response = await fetch('api/prompt');
+      const data = await response.json();
+      
+      setPosts(data);
+    }
+    fetchPosts()
+  },[])
   return (
     <section className="feed">
     <form className="relative w-full flex-center">
@@ -16,6 +40,10 @@ const Feed = () => {
         required className="search_input peer"
       />
     </form>      
+    <PromptCardList 
+    data={posts}
+    handleTagClick={()=>{}}
+    />
     </section>
   )
 }
