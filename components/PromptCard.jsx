682291cc2b7b194795615a7e0cbea976707475df
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-19 14:23:09 +0530

    feat(ui): finalize PromptCard component and implement copy prompt functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,6 +5,11 @@ import { useState } from "react";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("")
+  const handleClick = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(()=> setCopied(''), 1500);
+  }
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -21,7 +26,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
           </div>
         </div>
-        <div className="copy_btn" onClick={()=>{}}>
+        <div className="copy_btn" onClick={handleClick}>
          <Image src={copied=== post.prompt?'/assets/icons/tick.svg': '/assets/icons/copy.svg'} width={12} height={12}/>
         </div>
       </div>
