60dfaa17e09ba4ed4df471340aaa89dc76a3e513
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-21 09:48:08 +0530

    fix(auth): redirect to home page after sign-out to prevent unauthorized prompt creation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Nav = () => {
   const {data : session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     const setUpProviders = async () => {
@@ -18,6 +20,10 @@ const Nav = () => {
     };
     setUpProviders();
   }, []);
+
+  useEffect(()=>{
+    router.replace('/')
+  },[providers])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -13,9 +13,9 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const pathName = usePathname();
 
   const handleProfileClick = () => {
-    if(post.creator._id === session?.user.id)return router.push("/profile")
+    if(post.creator?._id === session?.user.id)return router.push("/profile")
 
-      router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+      router.push(`/profile/${post.creator?._id}?name=${post.creator?.username}`);
   }
 
   const handleClick = () => {
@@ -31,7 +31,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           onClick={handleProfileClick}
         >
           <Image
-            src={post.creator.image}
+            src={post.creator?.image}
             alt="user image"
             width={40}
             height={40}
@@ -39,10 +39,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.username}
+              {post.creator?.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
+              {post.creator?.email}
             </p>
           </div>
         </div>
@@ -66,7 +66,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         #{post.tag}
       </p>
 
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
+      {session?.user.id === post.creator?._id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
             className="font-inter text-sm green_gradient cursor-pointer"
